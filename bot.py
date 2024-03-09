4228c60a7163b1da76a060f9c347eb0b49aea9ef
Timothy Landry <31513409+TimothyLandry@users.noreply.github.com>
2024-03-09 17:22:46 -0500

    Added console prints on ready/cron

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ config = json.load(open("./config.json"))
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = discord.Client(intents=intents)
 
 @client.event
@@ -19,14 +18,14 @@ async def on_ready():
     logo = logo.replace("VERSION_REPLACE", config["version"])
     logo = logo.replace("TIMESTAMP_PLACE", time.strftime("%Y-%m-%d %H:%M:%S"))
 
-
     channel = client.get_channel(config["channelId"])
     await channel.send(logo)
-    cron_leaderboard.start()
+    cronLeaderboard.start()
+    print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Connected on {client.user}")
 
 @tasks.loop(hours=1)
-async def cron_leaderboard():
-    print('Starting cron_leaderboard')
+async def cronLeaderboard():
+    print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - cronLeaderboard start")
     channel = client.get_channel(config["channelId"])
 
     output = await asyncio.to_thread(getLeaderboardOutput)
