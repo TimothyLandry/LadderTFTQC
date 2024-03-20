635bbbdfb98dd7deb81977dbae463b52f99b9ff2
Timothy Landry <31513409+TimothyLandry@users.noreply.github.com>
2024-03-20 14:17:02 -0400

    Changing cron to 3h for the first few days

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ async def on_ready():
     cronLeaderboard.start()
     print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - Connected on {client.user}")
 
-@tasks.loop(hours=6)
+@tasks.loop(hours=3)
 async def cronLeaderboard():
     print(f"{time.strftime('%Y-%m-%d %H:%M:%S')} - cronLeaderboard start")
     channel = client.get_channel(config["channelId"])
